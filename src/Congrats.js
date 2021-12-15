83fd1cdada4509a26b04d1f0314562d709f88180
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-12-15 15:03:34 +0530

    testing congrats with success

--- src/Congrats.js
+++ src/Congrats.js
@@ -7,6 +7,16 @@ import React from "react";
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
 
-export default () => {
-  return <div>hello</div>;
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="component-message">
+          congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
