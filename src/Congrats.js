c88b456ea1f95dbcce05699a7c5b10318a6e8ff2
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-12-15 14:47:41 +0530

    testing congrats

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,6 +3,7 @@ import React from "react";
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props- React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
 
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -18,13 +18,19 @@ const setup = (props={})=> {
     return shallow(<Congrats {...props} />)
 }
 test("renders without error", () => {
-
+  const wrapper= setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
 });
 
 test("renders no text when success prop is false", () => {
-
+    const wrapper= setup({success:false});
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.text()).toBe('');
 });
 
 test("renders non-empty congrats message when success is true", () => {
-
+    const wrapper= setup({success:false});
+    const message = findByTestAttr(wrapper, 'component-message');
+    expect(message.text().length).not.toBe(0);
 });
