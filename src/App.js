e55acc216a3cfed8ea0a61577f362d41f879621b
the-manpreet-singh <manpreet.singh10196@gmail.com>
2021-12-15 12:51:45 +0530

    app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+
+// function App() {
+// const [count, setCount] = React.useState(0);
+class App extends Component {
+  render() {
+    return <div data-test="component-app" className="App"></div>;
+  }
+}
+
+export default App;
